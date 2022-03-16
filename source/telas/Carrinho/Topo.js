46c8868e8ae00dc178952cff1d4fcfd3247b094b
Janier Silva <janierfsilva@gmail.com>
2022-03-16 19:24:55 -0400

    arrumando arquivos 2

--- source/telas/Carrinho/Topo.js
+++ source/telas/Carrinho/Topo.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import { Image, Text, StyleSheet ,Dimensions } from 'react-native';
+import { Image, Text, StyleSheet, Dimensions } from 'react-native';
 import topo from '../../../assets/topo.png';
 import Texto from '../../components/Texto';
 
 
 const width = Dimensions.get("screen").width;
 
-export default function Topo (){
-    return  <>
-                <Image style={styles.topo} source={topo}/>
-                <Texto style= {styles.tituloCarrinho}>Carrinho</Texto>
-            </>
+export default function Topo() {
+  return <>
+    <Image style={styles.topo} source={topo} />
+    <Texto style={styles.tituloCarrinho}>Carrinho</Texto>
+  </>
 }
 const styles = StyleSheet.create({
-    topo:{
-        width:"100%",
-        height: 582/772 * width
-      },
-      tituloCarrinho:{
-        position:"absolute",
-        paddingVertical: 60,
-        textAlign:"center",
-        width:"100%",
-        fontSize: 20,
-        fontWeight: "bold",
-        lineHeight: 40,
-        
-      },
+  topo: {
+    width: "100%",
+    height: 582 / 772 * width
+  },
+  tituloCarrinho: {
+    position: "absolute",
+    paddingVertical: 60,
+    textAlign: "center",
+    width: "100%",
+    fontSize: 20,
+    fontWeight: "bold",
+    lineHeight: 40,
+
+  },
 })
