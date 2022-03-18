2c4ec8dad0d6759b79ab49955120e08bae6b2422
Janier Silva <janierfsilva@gmail.com>
2022-03-18 13:30:08 -0400

    criação do arquivo Itens para a lista parte 2

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import Topo from './source/telas/Carrinho/Topo';
 import Detalhes from './source/telas/Carrinho/Detalhes';
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 import Mocks from './source/mocks/carrinho'
+import Itens from './source/telas/Carrinho/Itens';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -18,6 +19,7 @@ export default function App() {
     <SafeAreaView style={styles.container}>
       <Topo {...Mocks.topo} />
       <Detalhes {...Mocks.detalhes}/> 
+      <Itens {...Mocks.itens}/>
                  
       <StatusBar  />
     </SafeAreaView>
---
+++ source/telas/Carrinho/Itens.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Texto from '../../components/Texto';
+
+export default function Itens({titulo, lista}){
+    return <>
+        <Texto>
+            {titulo}
+        </Texto>
+    </>;
+}
